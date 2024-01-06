e3345e156e713c00cdd7fcfed95c045862c4210b
James <james.hinton@live.co.uk>
2024-01-06 14:05:43 +0000

    feat: added webgl fallback

---
+++ src/apps/Room/components/ErrorBoundary/ErrorBoundary.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import "./ErrorBoundary.scss";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, errorMessage: "" };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true, errorMessage: error.message };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You might also log the error to an error reporting service
+    console.log(error, errorInfo);
+  }
+  render() {
+    if (this.state.hasError) {
+      // User-friendly error message
+      return (
+        <div style={{ padding: "20px", textAlign: "center" }}>
+          <h1>Oops! Something went wrong.</h1>
+          <p>It looks like WebGL 2.0 might not be enabled on your browser.</p>
+          <br />
+          <p>
+            WebGL is a technology that allows this application to display 3D
+            graphics. Without it, you can't see my 3D Room.
+          </p>
+          <br />
+          <h3>How to Enable WebGL:</h3>
+          <p>
+            Please check the following links to learn how to enable WebGL on
+            your browser:
+          </p>
+          <ul className="error-list">
+            <li>
+              <a
+                href="https://get.webgl.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                General WebGL Help
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://support.smartbear.com/crossbrowsertesting/docs/live-testing/enable-webgl-on-chrome.html"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Enable WebGL in Google Chrome
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://help.interplaylearning.com/en/help/how-to-enable-webgl-in-firefox"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Enable WebGL in Mozilla Firefox
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://discussions.apple.com/thread/8655829?sortBy=best"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Enable WebGL in Safari
+              </a>
+            </li>
+          </ul>
+          <br />
+          <br />
+          <p>
+            If you're still having issues after enabling WebGL, your device or
+            browser might not support it, or there could be another issue.{" "}
+            <br />
+            <strong>
+              I know that a lot of browsers have been disabling WebGL due to
+              security concerns.
+            </strong>
+          </p>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- src/apps/Room/index.jsx
+++ src/apps/Room/index.jsx
@@ -11,44 +11,61 @@ import "./About.css";
 // Components
 import Model from "./components/Model/Model";
 import Loader from "./components/Loader/Loader";
-
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 
 const Room = () => {
+  function isWebGLAvailable() {
+    try {
+      const canvas = document.createElement("canvas");
+      return !!(
+        window.WebGLRenderingContext &&
+        (canvas.getContext("webgl") || canvas.getContext("experimental-webgl"))
+      );
+    } catch (e) {
+      return false;
+    }
+  }
+
+  if (!isWebGLAvailable()) {
+    return <div>Please enable WebGL to use this feature.</div>;
+  }
+
   return (
     <>
       <div id="canvas-container">
+        <ErrorBoundary>
+          <Canvas camera={{ rotation: [1, 9, 5], position: [-45, 45, 80] }}>
+            <Suspense fallback={<Loader />}>
+              <Environment preset="forest" background />
 
-        <Canvas camera={{ rotation: [1, 9, 5], position: [-45, 45, 80] }}>
-          <Suspense fallback={<Loader />}>
-            <Environment preset="forest" background />
+              <Model
+                path={"/models/room/scene.gltf"}
+                scale={30}
+                position={[0, 0, -20]}
+              />
 
-            <Model
-              path={"/models/room/scene.gltf"}
-              scale={30}
-              position={[0, 0, -20]}
-            />
+              <Model
+                path={"/models/robot/scene.gltf"}
+                scale={3}
+                position={[0, 18, -23]}
+              />
 
-            <Model
-              path={"/models/robot/scene.gltf"}
-              scale={3}
-              position={[0, 18, -23]}
-            />
+              <Model
+                path={"/models/linux/scene.gltf"}
+                scale={0.1}
+                position={[-1.5, 15, 17]}
+              />
 
-            <Model
-              path={"/models/linux/scene.gltf"}
-              scale={0.1}
-              position={[-1.5, 15, 17]}
-            />
+              <Model
+                path={"/models/chess/scene.gltf"}
+                scale={0.015}
+                position={[-17, 10.5, 20]}
+              />
 
-            <Model
-              path={"/models/chess/scene.gltf"}
-              scale={0.015}
-              position={[-17, 10.5, 20]}
-            />
-
-            <OrbitControls autoRotate autoRotateSpeed={0.3} />
-          </Suspense>
-        </Canvas>
+              <OrbitControls autoRotate autoRotateSpeed={0.3} />
+            </Suspense>
+          </Canvas>
+        </ErrorBoundary>
       </div>
     </>
   );
