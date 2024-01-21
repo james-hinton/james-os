260b9737f331db9f213123b567f78a8cf33f3a70
James Hinton <63542818+james-hinton@users.noreply.github.com>
2024-01-21 14:57:43 +0000

    Add fade-out animation to Island component

--- src/components/TopBar/components/Island/Island.jsx
+++ src/components/TopBar/components/Island/Island.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import { PhoneContext } from "../../../../context/PhoneContext";
 
 // Icons
@@ -9,6 +9,7 @@ import "./Island.scss";
 
 const Island = () => {
   const { phoneLocked } = useContext(PhoneContext);
+  const islandRef = useRef(null);
   const [currentTime, setCurrentTime] = useState(
     new Date().toLocaleTimeString([], {
       hour: "2-digit",
@@ -29,6 +30,27 @@ const Island = () => {
     return () => clearInterval(interval);
   }, []);
 
+  // have a delay for after phone unlocked, so we can show a fade out animation
+  const [unMountIsland, setUnMountIsland] = useState(false);
+  useEffect(() => {
+    if (!phoneLocked) {
+      setTimeout(() => {
+        setUnMountIsland(true);
+      }, 1000);
+    }
+  }, [phoneLocked]);
+
+  // Add the fadeout class to islandRef and then remove it and put fade-in class
+  useEffect(() => {
+    if (!phoneLocked) {
+      islandRef.current.classList.add("fade-out");
+      setTimeout(() => {
+        islandRef.current.classList.remove("fade-out");
+        islandRef.current.classList.add("fade-in");
+      }, 1000);
+    }
+  }, [phoneLocked]);
+
   return (
     <div
       className="island-container"
@@ -40,16 +62,17 @@ const Island = () => {
         className={
           "island" + (phoneLocked ? " island-locked" : "island-unlocked")
         }
+        ref={islandRef}
       >
-        {phoneLocked ? (
-          <>
+        {!unMountIsland ? (
+          <div className={`${!phoneLocked && "fade-out"}`}>
             <div className="island__lock">
-              <LockIcon />
+              <LockIcon style={{ display: phoneLocked ? "block" : "none" }} />
             </div>
             <div className="island__text">James OS</div>
-          </>
+          </div>
         ) : (
-          <div className="island__text island__text-unlocked">
+          <div className="island__text island__text-unlocked fade-in">
             {currentTime}
           </div>
         )}
